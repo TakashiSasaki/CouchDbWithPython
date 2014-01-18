109b8e581d49fe5b9753cd8d2662e2ef5d596191
Takashi SASAKI <takashi316@gmail.com>
2014-01-18 19:28:23 +0900

    implemented getUuids, getStats and getLog

--- CouchDbHttpApi.py
+++ CouchDbHttpApi.py
@@ -24,8 +24,30 @@ class CouchDbHttpApiBase():
         print (r.getheaders())
         print (self.cookieJar)
         
+    def getUuids(self):
+        r = self.openerDirector.open("http://127.0.0.1:5984/_uuids")
+        print (r.status)
+        print (r.read())
+        print (r.getheaders())
+        print (self.cookieJar)
+
+    def getStats(self):
+        r = self.openerDirector.open("http://127.0.0.1:5984/_stats")
+        print (r.status)
+        print (r.read())
+        print (r.getheaders())
+
+    def getLog(self):
+        r = self.openerDirector.open("http://127.0.0.1:5984/_log")
+        print (r.status)
+        print (r.read())
+        print (r.getheaders())
 
 if __name__=="__main__":
     couch_db_http_api_base = CouchDbHttpApiBase()
     couch_db_http_api_base.getMotd()
     couch_db_http_api_base.getAllDbs()
+    couch_db_http_api_base.getUuids()
+    couch_db_http_api_base.getStats()
+    couch_db_http_api_base.getLog()
+    couch_db_http_api_base.saveCookie()
