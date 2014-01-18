f46fb4989771ce3c9d3606bd71620a64be8ae3da
Takashi SASAKI <takashi316@gmail.com>
2014-01-18 17:37:41 +0900

    example for urllib.request.urlopen

--- test.py
+++ test.py
@@ -7,5 +7,16 @@ def testHttpClient():
     print (http_response.read())
     print (http_response.getheaders())
 
+def testUrllib():
+    import urllib
+    f = urllib.request.urlopen("http://www.yahoo.co.jp/index.html")
+    for x in f:
+        print (x)
+
 if __name__=="__main__":
+    import sys
+    print ("Python %s.%s.%s"
+           % (sys.version_info.major, sys.version_info.minor,
+              sys.version_info.micro))
     testHttpClient()
+    testUrllib()
