569a56e2b12ed260aac40b4f09bae59e36182c83
Takashi SASAKI <takashi316@gmail.com>
2014-01-18 20:39:43 +0900

    loading and saving all cookies

--- CouchDbHttpApi.py
+++ CouchDbHttpApi.py
@@ -6,7 +6,7 @@ class CouchDbHttpApiBase():
         self.openerDirector = urllib.request.build_opener(self.cookieProcessor)
         print(self.openerDirector)
         try:
-            self.cookieJar.load()
+            self.cookieJar.load(ignore_discard=True, ignore_expires=True)
         except FileNotFoundError:
             pass
 
@@ -14,48 +14,48 @@ class CouchDbHttpApiBase():
         self.cookieJar.save(ignore_discard=True, ignore_expires=True)
 
     def getMotd(self):
-        r = self.openerDirector.open("http://127.0.0.1:5984/")
-        print (r.status)
-        print (r.read())
-        print (r.getheaders())
-        print (self.cookieJar)
+        self.open("/")
+        print (self.status())
+        print (self.read())
+        print (self.getheaders())
 
     def getAllDbs(self):
-        r = self.openerDirector.open("http://127.0.0.1:5984/_all_dbs")
-        print (r.status)
-        print (r.read())
-        print (r.getheaders())
-        print (self.cookieJar)
+        self.open("/_all_dbs")
+        print (self.status())
+        print (self.read())
+        print (self.getheaders())
         
     def getUuids(self):
-        r = self.openerDirector.open("http://127.0.0.1:5984/_uuids")
-        print (r.status)
-        print (r.read())
-        print (r.getheaders())
-        print (self.cookieJar)
+        self.open("/_uuids")
+        print (self.status())
+        print (self.read())
+        print (self.getheaders())
 
     def getStats(self):
-        r = self.openerDirector.open("http://127.0.0.1:5984/_stats")
-        print (r.status)
-        print (r.read())
-        print (r.getheaders())
+        self.openerDirector.open("/_stats")
+        print (self.status())
+        print (self.read())
+        print (self.getheaders())
 
     def open(self, path, dict_or_list_of_tuple=None):
         if dict_or_list_of_tuple is not None:
             data = urllib.parse.urlencode(dict_or_list_of_tuple)
-            self.httpResponse = self.openerDirector.open("http://127.0.0.1:5984/%s" % path, data=data.encode("utf-8"))
+            self.httpResponse = self.openerDirector.open("http://127.0.0.1:5984%s" % path, data=data.encode("utf-8"))
         else:
-            self.httpResponse = self.openerDirector.open("http://127.0.0.1:5984/%s" % path)
+            self.httpResponse = self.openerDirector.open("http://127.0.0.1:5984%s" % path)
         return self.httpResponse
 
     def status(self):
-        return self.httpResponse.status()
+        return self.httpResponse.status
 
     def read(self):
         return self.httpResponse.read()
 
+    def getheaders(self):
+        return self.httpResponse.getheaders()
+
     def getLog(self):
-        self.open("_log")
+        self.open("/_log")
         #print (self.read())
 
     def postSession(self):
@@ -68,12 +68,6 @@ if __name__=="__main__":
     #couch_db_http_api_base.getAllDbs()
     #couch_db_http_api_base.getUuids()
     #couch_db_http_api_base.getStats()
-    print (couch_db_http_api_base.cookieJar)
-    #couch_db_http_api_base.cookieJar.load("cookie.txt")
-    print (couch_db_http_api_base.cookieJar)
-    couch_db_http_api_base.postSession()
-    print (couch_db_http_api_base.cookieJar)
+    #couch_db_http_api_base.postSession()
     couch_db_http_api_base.getLog()
     couch_db_http_api_base.saveCookie()
-    couch_db_http_api_base.cookieJar.load()
-    print (couch_db_http_api_base.cookieJar)
