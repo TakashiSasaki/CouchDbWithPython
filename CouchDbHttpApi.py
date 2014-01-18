70d5169443dd7ae3df586c3abfd5e61e074a2c0b
Takashi SASAKI <takashi316@gmail.com>
2014-01-18 20:31:06 +0900

    implemented cookie authentication

--- CouchDbHttpApi.py
+++ CouchDbHttpApi.py
@@ -1,14 +1,17 @@
 import http.cookiejar, urllib.request
 class CouchDbHttpApiBase():
     def __init__(self):
-        self.cookieJar = http.cookiejar.LWPCookieJar()
+        self.cookieJar = http.cookiejar.LWPCookieJar('cookie.txt')
         self.cookieProcessor = urllib.request.HTTPCookieProcessor(self.cookieJar)
         self.openerDirector = urllib.request.build_opener(self.cookieProcessor)
         print(self.openerDirector)
-        self.cookieJar.load("cookie.txt")
+        try:
+            self.cookieJar.load()
+        except FileNotFoundError:
+            pass
 
     def saveCookie(self):
-        self.cookieJar.save("cookie.txt")
+        self.cookieJar.save(ignore_discard=True, ignore_expires=True)
 
     def getMotd(self):
         r = self.openerDirector.open("http://127.0.0.1:5984/")
@@ -37,17 +40,40 @@ class CouchDbHttpApiBase():
         print (r.read())
         print (r.getheaders())
 
+    def open(self, path, dict_or_list_of_tuple=None):
+        if dict_or_list_of_tuple is not None:
+            data = urllib.parse.urlencode(dict_or_list_of_tuple)
+            self.httpResponse = self.openerDirector.open("http://127.0.0.1:5984/%s" % path, data=data.encode("utf-8"))
+        else:
+            self.httpResponse = self.openerDirector.open("http://127.0.0.1:5984/%s" % path)
+        return self.httpResponse
+
+    def status(self):
+        return self.httpResponse.status()
+
+    def read(self):
+        return self.httpResponse.read()
+
     def getLog(self):
-        r = self.openerDirector.open("http://127.0.0.1:5984/_log")
-        print (r.status)
-        print (r.read())
-        print (r.getheaders())
+        self.open("_log")
+        #print (self.read())
+
+    def postSession(self):
+        self.open("_session", {'name': "admin", 'password': 'adminpass'})
+        #print(self.httpResponse.read())
 
 if __name__=="__main__":
     couch_db_http_api_base = CouchDbHttpApiBase()
     couch_db_http_api_base.getMotd()
-    couch_db_http_api_base.getAllDbs()
-    couch_db_http_api_base.getUuids()
-    couch_db_http_api_base.getStats()
+    #couch_db_http_api_base.getAllDbs()
+    #couch_db_http_api_base.getUuids()
+    #couch_db_http_api_base.getStats()
+    print (couch_db_http_api_base.cookieJar)
+    #couch_db_http_api_base.cookieJar.load("cookie.txt")
+    print (couch_db_http_api_base.cookieJar)
+    couch_db_http_api_base.postSession()
+    print (couch_db_http_api_base.cookieJar)
     couch_db_http_api_base.getLog()
     couch_db_http_api_base.saveCookie()
+    couch_db_http_api_base.cookieJar.load()
+    print (couch_db_http_api_base.cookieJar)
