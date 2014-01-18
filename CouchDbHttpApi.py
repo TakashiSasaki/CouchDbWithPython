64b13b3ee98f4dd1fa993129ef985bbad102f5bb
Takashi SASAKI <takashi316@gmail.com>
2014-01-18 19:13:41 +0900

    implemented motd

---
+++ CouchDbHttpApi.py
@@ -0,0 +1,21 @@
+import http.cookiejar, urllib.request
+class CouchDbHttpApiBase():
+    def __init__(self):
+        self.cookieJar = http.cookiejar.LWPCookieJar()
+        self.cookieProcessor = urllib.request.HTTPCookieProcessor(self.cookieJar)
+        self.openerDirector = urllib.request.build_opener(self.cookieProcessor)
+        print(self.openerDirector)
+        self.cookieJar.load("cookie.txt")
+
+    def saveCookie(self):
+        self.cookieJar.save("cookie.txt")
+
+    def getMotd(self):
+        r = self.openerDirector.open("http://127.0.0.1:5984/")
+        print (r.status)
+        print (r.getheaders())
+        print (self.cookieJar)
+
+if __name__=="__main__":
+    couch_db_http_api_base = CouchDbHttpApiBase()
+    couch_db_http_api_base.getMotd()
