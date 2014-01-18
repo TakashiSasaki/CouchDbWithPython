e5ec50fbb87c686ab4ffe6b2991e30484bce019c
Takashi SASAKI <takashi316@gmail.com>
2014-01-18 19:17:46 +0900

    implemented getAllDbs

--- CouchDbHttpApi.py
+++ CouchDbHttpApi.py
@@ -13,9 +13,19 @@ class CouchDbHttpApiBase():
     def getMotd(self):
         r = self.openerDirector.open("http://127.0.0.1:5984/")
         print (r.status)
+        print (r.read())
         print (r.getheaders())
         print (self.cookieJar)
+
+    def getAllDbs(self):
+        r = self.openerDirector.open("http://127.0.0.1:5984/_all_dbs")
+        print (r.status)
+        print (r.read())
+        print (r.getheaders())
+        print (self.cookieJar)
+        
 
 if __name__=="__main__":
     couch_db_http_api_base = CouchDbHttpApiBase()
     couch_db_http_api_base.getMotd()
+    couch_db_http_api_base.getAllDbs()
